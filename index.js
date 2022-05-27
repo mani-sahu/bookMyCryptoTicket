0ec1a075ca87dd6bf76303e43d916a12a788185f
Amarnath <amarnathk5674@gmail.com>
2022-05-27 18:12:55 +0530

    mongo db added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const res = require('express/lib/response');
 
 const app = express();
 
+connectDB();
 
+app.get('/',(req,res) => res.send('Hello World!') );
 
+app.listen(8082,() => {
+    console.log('Server is listening on port 3000')
+});
